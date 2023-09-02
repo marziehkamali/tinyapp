1c6278b14af906f2ce81b6e8917764ecb93808b0
marziehkamali <mayhey1@gmail.com>
2023-09-02 10:36:23 -0600

    Add GET route to expressserver.js and render using accompanying template. Add POST route to expressserver.js to receive form submission

--- express_server.js
+++ express_server.js
@@ -9,6 +9,8 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -30,7 +32,28 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]};
   res.render("urls_show", templateVars);
 });
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
+
+function generateRandomString() {
+  let alphaNum = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  let randomStr = "";
+  for(let i=0; i < 6; i++){
+    let result = Math.floor(Math.random() * 35);
+    randomStr = randomStr.concat(alphaNum[result]);
+  }
+  return randomStr;
+}
+
+console.log(generateRandomString());
